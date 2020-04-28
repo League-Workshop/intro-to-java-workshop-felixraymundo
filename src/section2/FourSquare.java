4a494df01b69b2d8fd605e2d305aaed3ef32b067
root <root@11874f273320>
2020-04-28 23:54:16 +0000

    Robot2(FourSquare)

--- src/section2/FourSquare.java
+++ src/section2/FourSquare.java
@@ -6,28 +6,34 @@ import org.jointheleague.graphical.robot.Robot;
 public class FourSquare {
 	
 	// 2. Create a new Robot
-
+	Robot robot = new Robot();
 	void go() {
 		// 4. Make the robot move as fast as possible
-
+		robot.penDown();
+		robot.setSpeed(100);
 		// 5. Set the pen width to 5
-
+		robot.setPenWidth(5);
 		// 6. Use a for loop to repeat steps #7 to #8, four times...
-
-			// 7. Set the pen color to random
-	
-			// 1. Call the drawSquare() method
-	
-			// 8. Turn the robot 90 degrees to the right
-
+			for() {
+				// 7. Set the pen color to random
+				setRandomPenColor();
+				// 1. Call the drawSquare() method
+				drawSquare();
+				// 8. Turn the robot 90 degrees to the right
+				robot.turn(90);
+			}
 	}
 
 	
 	void drawSquare() {
 		JOptionPane.showMessageDialog(null, "yay! you called the drawSquare() method!");
 		/* 3. Fill in the code to draw a square inside the method below. */
-		
-		
+		robot.move(100);
+		robot.turn(90);
+		robot.move(100);
+		robot.turn(90);
+		robot.move(100);
+		robot.turn(90);
 	}
 
 	public static void main(String[] args) {
--- src/section2/MyFirstJavaProgram.java
+++ src/section2/MyFirstJavaProgram.java
@@ -9,11 +9,24 @@ public class MyFirstJavaProgram {
 		
 		Robot robot = new Robot();
 		robot.penDown();
+		robot.setSpeed(100);
 		robot.setPenColor(Color.blue);
 		
-		robot.move(100);
-		robot.turn(90);//<--- Robot se mueve derecha 90 grados
-		robot.move(100);
+//		robot.move(100);
+//		robot.turn(90);//<--- Robot se mueve derecha 90 grados
+//		robot.move(100);
+//		robot.turn(90);
+//		robot.move(100);
+//		robot.turn(90);
+//		robot.move(100);
+//		robot.turn(90);
+		
+		for( int i = 0; i < 4; i++) {    // <
+			robot.move(100);
+			robot.turn(90);
+		}
+		
+	
 	
 		
 	}
