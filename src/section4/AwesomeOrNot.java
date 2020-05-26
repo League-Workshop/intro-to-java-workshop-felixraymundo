0ef6b8f698747d9997a16bebd009822769a55a34
root <root@7ede569b8c92>
2020-05-26 23:56:14 +0000

    QuizGame, Candy Crush.

--- src/section4/AwesomeOrNot.java
+++ src/section4/AwesomeOrNot.java
@@ -1,33 +1,57 @@
 package section4;
 
+import java.awt.SystemColor;
 import java.util.Random;
+
+import javax.swing.JOptionPane;
+
+import com.sun.corba.se.spi.orbutil.fsm.Input;
+import com.sun.org.apache.xpath.internal.operations.Variable;
+
+import sun.awt.AWTAccessor.SystemColorAccessor;
 
 public class AwesomeOrNot {
 
 	// 1. Make a main method that includes all the steps below…
 
+	public static void main(String[] args) {
+		
+	
 	// 2. Make a variable that will hold a random number and put a random number into this variable using "new Random().nextInt(4)"
-
+		
+		int number = new Random().nextInt(4);
 	// 3. Print out this variable
-
+		System.out.println(number);
 	// 4. Get the user to enter something that they think is awesome
-
+		JOptionPane.showInputDialog(null, "What, in your opinion, is awesome?");	
 	// 5. If the random number is 0
-
-	// -- tell the user whatever they entered is awesome!
-
+		if ( number == 0) {
+	// -- tell the user watever they entered is awesome!
+		JOptionPane.showMessageDialog(null, "That is awesome!");
+		}
 	// 6. If the random number is 1
-
+		if ( number == 1) {
 	// -- tell the user whatever they entered is ok.
-
-	// 7. If the random number is 2
-
+		JOptionPane.showMessageDialog(null, "That is ok");
+		}
+		// 7. If the random number is 2
+		if ( number == 2) { 
 	// -- tell the user whatever they entered is boring.
-
+		JOptionPane.showMessageDialog(null, "That is boring");
+		}
 	// 8. If the random number is 3
-
+		if ( number == 3) {
 	// -- write your own answer
+		JOptionPane.showMessageDialog(null, "I think Tacos are better. ");
+		
+		}
 
+	}
+
+	private static Random newRandom() {
+		// TODO Auto-generated method stub
+		return null;
+	}
 }
 
 
--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -1,15 +1,17 @@
 package section4;
 
+import javax.swing.JOptionPane;
+
 public class QuizGame {
 	
 	public static void main(String[] args) {
 		
 		// 1.  Create a variable to hold the user's score 
-		
+		int score = 0;
 		// 2.  Ask the user a question 
-		
+		String input = JOptionPane.showInputDialog(null, "What is 2+2-4?");
 		// 3.  Use an if statement to check if their answer is correct
-		
+		if ( input.equals("0"));
 		// 4.  if the user's answer was correct, add one to their score 
 		
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
