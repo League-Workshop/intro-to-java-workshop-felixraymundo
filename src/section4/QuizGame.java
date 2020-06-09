a575e5ce860f9db4aca424ea47e520595d4f33e4
root <root@9b1c6381fe95>
2020-06-09 23:56:56 +0000

    QuizGame

--- src/section4/QuizGame.java
+++ src/section4/QuizGame.java
@@ -11,12 +11,41 @@ public class QuizGame {
 		// 2.  Ask the user a question 
 		String input = JOptionPane.showInputDialog(null, "What is 2+2-4?");
 		// 3.  Use an if statement to check if their answer is correct
-		if ( input.equals("0"));
+		if (input.equals("0")) {
+			
+		}
+		else {
+			
+		}
 		// 4.  if the user's answer was correct, add one to their score 
+		if (input.equals("0")) {
+			score += 1;
+			JOptionPane.showMessageDialog(null, "Your Score = " + score);
+			
+		}
+		else {
+			JOptionPane.showMessageDialog(null, "Your Score = " + score);
+			score = 0;
+		}
 		
 		// 5.  Create more questions by repeating steps 1, 2, and 3 below. 
-		
+		input = JOptionPane.showInputDialog(null, "What is 5+6-8+8");
 		// 6.  After all the questions have been asked, print the user's score 
+		if (input.equals("11")) {
+		score += 1;
+		JOptionPane.showMessageDialog(null, "Your Score = " + score);
 		
+		}
+		else {
+			score += 0;
+			JOptionPane.showMessageDialog(null, "Your Score = " + score);
+			
+		}
+		
+		else {
+			
+		score += 1;
+			JOptionPane.showMessageDialog(null, "Your Score = " + score);
+		}
 	}
 }
